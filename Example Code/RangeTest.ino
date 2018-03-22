573b4ef636703b39079b4a799f29bab1f2c43336
Ryan Wade <MIABot@gmail.com>
2018-03-22 13:40:32 -0600

    Updates
    Updated model designs and added example code for the sonic sensor to output range to an LCD display

---
+++ Example Code/RangeTest.ino
@@ -0,0 +1,51 @@
+/*************************************************** 
+  This is a library for our I2C LED Backpacks
+
+  Designed specifically to work with the Adafruit LED 7-Segment backpacks 
+  ----> http://www.adafruit.com/products/881
+  ----> http://www.adafruit.com/products/880
+  ----> http://www.adafruit.com/products/879
+  ----> http://www.adafruit.com/products/878
+
+  These displays use I2C to communicate, 2 pins are required to 
+  interface. There are multiple selectable I2C addresses. For backpacks
+  with 2 Address Select pins: 0x70, 0x71, 0x72 or 0x73. For backpacks
+  with 3 Address Select pins: 0x70 thru 0x77
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+int distance;
+int AnalogValue;
+const int readPin = A0;
+
+
+void setup() {
+#ifndef __AVR_ATtiny85__
+  Serial.begin(9600);
+#endif
+  matrix.begin(0x70);
+  pinMode(readPin, INPUT);
+}
+
+void loop() {
+
+
+  AnalogValue = analogRead(readPin);
+  // print with print/println
+  matrix.print(int(AnalogValue/2.54));
+  matrix.writeDisplay();
+  Serial.print(AnalogValue);
+  Serial.print(" ");              
+  Serial.println(distance);
+  delay(250); 
